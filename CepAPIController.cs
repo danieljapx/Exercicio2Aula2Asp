3ab29b588cd29634d71744a99efb41eb7363bef1
danieljapx <66913258+danieljapx@users.noreply.github.com>
2020-07-18 18:38:04 -0300

    Atividade 2 - API Controller
    Daniel Hideki - Atividade 2 Aula Presencial 2 ASP.net

---
+++ CepAPIController.cs
@@ -0,0 +1,34 @@
+﻿using Atividade1Aula2Daniel.DAL;
+using Atividade1Aula2Daniel.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Atividade1Aula2Daniel.Controllers
+{
+    [Route("api/Endereco")]
+    [ApiController]
+    public class CepAPIController : ControllerBase
+    {
+        private readonly CepDAO _cepDao;
+
+
+
+        public CepAPIController(CepDAO cepDao)
+        {
+            _cepDao = cepDao;
+        }
+
+        //Inicio da Implementação dos Métodos
+
+        //GET: /api/Endereco/ListarEnderecos
+        [HttpGet]
+        [Route("ListarEnderecos")]
+        public IActionResult ListarEnderecos()
+        {
+            return Ok(_cepDao.Listar());
+        }
+
+        
+
+    }
+}
